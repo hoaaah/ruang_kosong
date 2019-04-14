571f1d2bdfcfdc772fb4e2c4ecfd1f2f41d365eb
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2019-04-14 23:12:56 +0700

    Bookguna Form

--- protected/models/Bookguna.php
+++ protected/models/Bookguna.php
@@ -31,74 +31,86 @@ class Bookguna extends CFormModel
 	public $col20;
 	public $col21;
 	public $col22;
-	public $weeks;
+	public $weeks, $dari, $jumlah_peserta, $penanggung_jawab, $konsumsi, $tor_kak, $approver, $approval_date, $catatan, $yang_mengajukan;
 
 	public function rules()
 	{
-		return array(
+		return [
 			// name, email, subject and body are required
-			array('kelas_id, mata_kuliah, tanggal_guna', 'required'),
-			array(' col1,
-				    col2,
-					col3,
-					col4,
-					col5,
-					col6,
-					col7,
-					col8,
-					col9,
-					col10,
-					col11,
-					col12,
-					col13,
-					col14,
-					col15,
-					col16,
-					col17,
-					col18,
-					col19,
-					col20,
-					col21,
-					col22, weeks,',
-					'numerical', 'integerOnly'=>true
-					//'boolean'
-					),
-			array('kelas_id, gedung_id, mata_kuliah,col1,
-				    col2,
-					col3,
-					col4,
-					col5,
-					col6,
-					col7,
-					col8,
-					col9,
-					col10,
-					col11,
-					col12,
-					col13,
-					col14,
-					col15,
-					col16,
-					col17,
-					col18,
-					col19,
-					col20,
-					col21,
-					col22, weeks, tanggal_guna', 'safe', 'on'=>'search'),
-		);
+			['kelas_id, mata_kuliah, tanggal_guna', 'required'],
+			[' col1,
+				col2,
+				col3,
+				col4,
+				col5,
+				col6,
+				col7,
+				col8,
+				col9,
+				col10,
+				col11,
+				col12,
+				col13,
+				col14,
+				col15,
+				col16,
+				col17,
+				col18,
+				col19,
+				col20,
+				col21,
+				col22, weeks,',
+				'numerical', 'integerOnly'=>true
+				//'boolean'
+			],
+			['kelas_id, gedung_id, mata_kuliah,col1,
+				col2,
+				col3,
+				col4,
+				col5,
+				col6,
+				col7,
+				col8,
+				col9,
+				col10,
+				col11,
+				col12,
+				col13,
+				col14,
+				col15,
+				col16,
+				col17,
+				col18,
+				col19,
+				col20,
+				col21,
+				col22, weeks, tanggal_guna', 'safe', 'on'=>'search'
+			],
+			['jumlah_peserta, konsumsi, tor_kak', 'numerical', 'integerOnly'=>true],
+			['dari, penanggung_jawab, approver, catatan, yang_mengajukan', 'length', 'max'=>200],
+			['approval_date', 'safe'],
+		];
 	}
 	
 	public function attributeLabels()
 	{
-		return array(
+		return [
 			'gedung_id' => 'Gedung',
 			'kelas_id' => 'Kelas',
 			'tanggal_guna' => 'Tanggal Mulai',
 			'session_id' => 'sesi',
-			'mata_kuliah' => 'Mata Kuliah',
-			'weeks' => 'Jumlah Minggu',
-			
-		);
+			'mata_kuliah' => 'Kegiatan',
+			'weeks' => 'Jumlah Hari',	
+			'dari' => 'Dari',
+			'jumlah_peserta' => 'Jumlah Peserta',
+			'penanggung_jawab' => 'Penanggung Jawab',
+			'konsumsi' => 'Konsumsi',
+			'tor_kak' => 'Tor Kak',
+			'approver' => 'Approver',
+			'approval_date' => 'Upproval Date',
+			'catatan' => 'Catatan',
+			'yang_mengajukan' => 'Yang Mengajukan',
+		];
 	}	
 
 }
